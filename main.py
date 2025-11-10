04bd38ad86b99472218550efa50ce4c345cdff1f
SujanKarki <sujandhoj.karki@gamail.com>
2025-11-10 08:25:46 +0545

    Added new

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("hello world")
-print("New Feature Added")
-print("save")
+
+import mariadb
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# 1. Database Connection Parameters
+db_config = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'your_username',
+    'password': 'your_password',
+    'database': 'your_database_name'
+}
